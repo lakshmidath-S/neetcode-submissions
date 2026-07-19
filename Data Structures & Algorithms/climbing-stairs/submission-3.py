28de791ca3a9b87d8e2a81f9c21f0e76d201a181
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-07-19 21:41:59 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp=[0]*(n+1)
+        def func(n):
+            if n<3:
+                dp[n]=n
+                return dp[n]
+            else:
+                if dp[n]!=0:
+                    return dp[n]
+                else:
+                    dp[n]=func(n-1)+func(n-2)
+                    return dp[n]
+        return func(n)
