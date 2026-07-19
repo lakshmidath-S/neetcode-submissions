c012db0ae151e72d64bb7070fb9cbb598197426e
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-07-19 13:32:55 +0530

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [[] for _ in range(9)]
+        col=len(board[0])
+        row=len(board)
+        for i in range(col):
+            cols=set()
+            for j in range(row):
+                if board[j][i]!='.':
+                    if board[j][i] in cols:
+                        return False 
+                    else: 
+                        cols.add(board[j][i])
+
+        for i in range(row):
+            rows=set()
+            for j in range(col):
+                if board[i][j]!='.':
+                    if board[i][j] in rows:
+                        return False 
+                    else:
+                        rows.add(board[i][j])
+        n=0
+        k=0
+        for n in range(3):
+            for k in range(3):
+                grid=set()
+                for i in range(3*n,3*(n+1)):
+                    for j in range(3*k,3*(k+1)):
+                        if board[i][j]!='.':
+                            if board[i][j] in grid:
+                                return False 
+                            else:
+                                grid.add(board[i][j])
+        return True
