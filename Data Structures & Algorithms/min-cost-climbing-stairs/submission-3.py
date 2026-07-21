1e5c1e07ac29c6e0a283fda63f1bd81968e7cc4e
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-07-21 23:25:19 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        mincost=0
+        target=len(cost)
+        dp=[float('inf')]*(target+1)
+        def climber(i):
+            if i>=target:
+                return 0
+            else:
+                if dp[i]!=float('inf'):
+                    return dp[i]
+                else:
+                    dp[i]=cost[i]+min(climber(i+1),climber(i+2))
+                    return dp[i]
+        return min(climber(0),climber(1))
