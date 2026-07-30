09b11770be28e6ddc451e80959d616e1bd33788f
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-07-30 21:46:38 +0530

    Add: plus-one - submission-2

---
+++ Data Structures & Algorithms/plus-one/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        carry=0
+        for i in range(len(digits)-1,-1,-1):
+            if i==len(digits)-1:
+                carry=1
+            total=digits[i]+carry
+            if total>9:
+                carry=1
+                digits[i]=total-10
+            else:
+                carry=0
+                digits[i]=total
+        if carry==1:
+            digits=[1]+digits
+        return digits
