902bfc3a402d095630020445b0a804208202f285
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-08-27 23:52:34 +0530

    Add: design-twitter-feed - submission-2

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-2.py
@@ -0,0 +1,47 @@
+class Twitter:
+
+    def __init__(self):
+        self.users=[]
+        self.following={}
+        self.tweets={}
+        self.tc=1
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        if userId not in self.users:
+            self.users.append(userId)
+            self.tweets[userId]=[]
+            self.following[userId]=set()
+        self.tweets[userId].append((self.tc,tweetId))
+        self.tc+=1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        if userId not in self.users:
+            self.users.append(userId)
+            self.tweets[userId]=[]
+            self.following[userId]=set()
+        ts=[]
+        count=0
+        newlist=[x for x in self.following[userId]]
+        newlist.append(userId)
+        for i in newlist:
+            jk=[]
+            for j in self.tweets[i]:
+                ts.append(j)
+        ts.sort(reverse=True)
+        m=[]
+        for i,j in ts:
+            if count==10:
+                break
+            m.append(j)
+            count+=1
+        return m
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId not in self.users:
+            self.users.append(followerId)
+            self.tweets[followerId]=[]
+            self.following[followerId]=set()
+        self.following[followerId].add(followeeId)
+        
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.following[followerId].discard(followeeId)
