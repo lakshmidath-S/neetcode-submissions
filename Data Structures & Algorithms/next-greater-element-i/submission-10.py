effafe0883256a093025fb7d6921b02fa3d2589f
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-09-25 23:06:12 +0530

    Add: next-greater-element-i - submission-10

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-10.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        freq={}
+        ans=[]
+        stack=[]
+        for i in nums2:
+            if not stack or stack[-1]>i:
+                stack.append(i)
+            else:
+                while stack and stack[-1]<i:
+                    a=stack.pop()
+                    freq[a]=i
+                stack.append(i)
+
+
+        for i in nums1:
+            if i in freq:
+                ans.append(freq[i])
+                continue
+            ans.append(-1)
+        return ans
+
+                
