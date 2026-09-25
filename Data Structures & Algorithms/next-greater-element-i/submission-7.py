69fafdb61c822042729d34ad1ac98bcd0a23c800
Lakshmidath  S <lakshmidath.s@gmail.com>
2026-09-25 22:58:32 +0530

    Add: next-greater-element-i - submission-7

---
+++ Data Structures & Algorithms/next-greater-element-i/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        freq={}
+        ans=[]
+        for i in range(len(nums2)):
+            for j in range(i+1,len(nums2)):
+                if nums2[j]>nums2[i]:
+                    if nums2[i] not in freq:
+                        freq[nums2[i]]=nums2[j]
+                        break
+        for i in nums1:
+            if i in freq:
+                ans.append(freq[i])
+                continue
+            ans.append(-1)
+        return ans
+
+                
